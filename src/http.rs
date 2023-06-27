0fbc91f936e845d647a1a7aa69055a26af6eb34d
igufei <igufei@126.com>
2023-06-27 17:51:47 +0800

    0627 测试版本

--- src/http.rs
+++ src/http.rs
@@ -1,15 +1,14 @@
-use std::collections::HashMap;
 
 pub async fn get(url: &str) -> String {
     let body = reqwest::get(url).await.unwrap().text().await.unwrap();
     body
 }
 
-pub async fn post(url: &str,map:&HashMap<String,String>) -> String {
+pub async fn post(url: &str, body: &str) -> String {
     let client = reqwest::Client::new();
     let body = client
         .post(url)
-        .json(&map)
+        .body(body.to_string())
         .send()
         .await
         .unwrap()
