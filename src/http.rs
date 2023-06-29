64466dbdd359996e6cc82114b8d91826dae80b5e
igufei <igufei@126.com>
2023-06-29 12:35:55 +0800

    0629 请求https网址必须要使用到openssl库,交叉编译时必须要加入openssl库,搞了五六天,真你麻超蛋

--- src/http.rs
+++ src/http.rs
@@ -1,10 +1,15 @@
 
 pub async fn get(url: &str) -> String {
     let body = reqwest::get(url).await.unwrap().text().await.unwrap();
+    
     body
 }
 
 pub async fn post(url: &str, body: &str) -> String {
+    //let res = surf::post(url).body_string(body.to_string()).recv_string().await.unwrap();
+    //hyper::Client::new();
+
+    //let res = "".to_string();
     let client = reqwest::Client::new();
     let body = client
         .post(url)
@@ -17,3 +22,5 @@ pub async fn post(url: &str, body: &str) -> String {
         .unwrap();
     body
 }
+
+
